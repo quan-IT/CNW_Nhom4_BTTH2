aabe42212733622708913e1b29462650d6c0e535
Minh <minhn6673@gmail.com>
2025-12-07 13:56:53 +0700

    update login js

--- views/auth/login.php
+++ views/auth/login.php
@@ -41,7 +41,7 @@
 						<input class="input100" type="text" name="email" placeholder="Email">
 						<span class="focus-input100"></span>
 						<span class="symbol-input100">
-							<i class="fa fa-envelope" aria-hidden="true"></i>
+							<i class="bi bi-envelope"></i>
 						</span>
 					</div>
 
