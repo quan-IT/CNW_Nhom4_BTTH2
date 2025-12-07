b81c67e5ece73f083062fffa2fd75d3405da5eee
quan <nguyenhuuquan01122k5@gmail.com>
2025-12-08 03:31:12 +0700

    fix EnrollmentController

--- controllers/EnrollmentController.php
+++ controllers/EnrollmentController.php
@@ -1,6 +1,30 @@
 <?php
 
+require_once 'models/Enrollment.php';
+
+require_once "config/database.php";
+$db = new Database();
+
 class EnrollmentController
 {
-    public $id
+    public $EnrollmentModel;
+
+    private $db;
+
+    public function __construct($db)
+    {
+        $this->db = $db;
+        $this->EnrollmentModel = new Enrollment($this->db);
+    }
+
+    // Đăng ký khóa học
+    public function register($user_id, $course_id)
+    {
+        if ($this->EnrollmentModel->isRegistered($user_id, $course_id)) {
+            return "Bạn đã đăng ký khóa học này!";
+        }
+
+        $success = $this->EnrollmentModel->register($user_id, $course_id);
+        return $success ? "Đăng ký thành công!" : "Đăng ký thất bại!";
+    }
 }
