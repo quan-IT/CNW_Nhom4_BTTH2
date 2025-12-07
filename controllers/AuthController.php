4630ff0e73873ae654a410ca3d0f34400da63ca5
Minh <minhn6673@gmail.com>
2025-12-07 12:28:22 +0700

    change route login, register

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -1,10 +1,10 @@
 <?php
 class AuthController{
   public function login(){
-    require "./CNW_Nhom4_BTTH2/views/auth/login.php";
+    require "./views/auth/login.php";
   }
 
   public function register(){
-    require "./CNW_Nhom4_BTTH2/views/auth/register.php";
+    require "./views/auth/register.php";
   }
 }
